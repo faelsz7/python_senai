b649422ec4204a3d7fc93b1e58167aa450ab2baf
raphael <seioqseioqla@gmail.com>
2024-08-21 14:46:55 -0300

    organizando diretorios

---
+++ aula04_a_aula10/aula06/aula06.py
@@ -0,0 +1,27 @@
+#candidato = int(input('informe o numero do candidato \n'))
+#
+#if candidato ==13:
+#    print('voutou no lua \n')
+#elif candidato == 22:
+#    print('votou no mito \n')
+#else:
+#    print('candidato invalido \n')
+#
+#candidato = int(input('informe o numero do candidato \n'))
+#
+#match candidato:
+#    case 13:
+#        print('voutou no lula')
+#    case 22:
+#        print('votou no mito')
+#    case _:
+#        print('invalido')
+#
+#--------------------------------------------------------------------
+#atribuir valores a uma variavel
+
+numero = 10
+print (numero)
+
+numero = numero + 10
+print(numero)
