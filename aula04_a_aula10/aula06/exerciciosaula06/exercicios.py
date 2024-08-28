b1bb832d91e4f52c7204a6b18901af3e35749f38
raphael <seioqseioqla@gmail.com>
2024-08-28 17:55:28 -0300

    revisao das aulas ate o dia 28

---
+++ aula04_a_aula10/aula06/exerciciosaula06/exercicios.py
@@ -0,0 +1,11 @@
+match numero:
+    case 1:
+        print('opção 1')
+    case 2:
+        print('opção 2')
+    case 3:
+        print('opção 3')
+    case 4:
+        print('opção 4')
+    case _:
+        print('invalido')
